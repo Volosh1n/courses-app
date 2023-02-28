36fed1a172055610822d89a5f6df9d7d1761c133
personal <seva.voloshin@gmail.com>
2023-02-28 14:37:27 +0200

    Add auth

--- src/common/Button/Button.jsx
+++ src/common/Button/Button.jsx
@@ -1,11 +1,14 @@
 const Button = (props) => {
   return (
     <>
-      <button className={props.buttonClassName} onClick={props.onButtonClick}>
+      <button
+        className={props.buttonClassName || 'btn btn-primary'}
+        onClick={props.onButtonClick}
+      >
         {props.buttonText}
       </button>
     </>
-  )
-}
+  );
+};
 
 export default Button;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import Logo from './components/Logo/Logo';
+import Auth from './components/Auth/Auth';
 
 const Header = () => {
   return (
-    <div className='border border-primary rounded p-3'>
+    <div className='d-flex justify-content-between align-items-center border border-primary rounded p-3'>
       <Logo />
+      <Auth />
     </div>
   );
 };
